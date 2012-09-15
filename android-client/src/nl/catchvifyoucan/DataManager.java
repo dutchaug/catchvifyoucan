1877451ff0e9f881699d98e82f8082329d15458c
Hans van der Scheer <Hans@ictranslate.nl>
2012-09-15 13:53:08 +0200

    Added the server class

---
+++ android-client/src/nl/catchvifyoucan/DataManager.java
@@ -0,0 +1,25 @@
+package nl.catchvifyoucan;
+
+import java.util.ArrayList;
+
+/**
+ * This class is responsible for the communication with the backendserver
+ * @author Hans
+ *
+ */
+public class DataManager {
+	
+	public DataManager(){
+		super();
+	}
+	
+	public void pushLocationToServer(int id, int latitude, int longitude){
+		
+	}
+	
+	public ArrayList<Integer> getHistoricalPosition(int id){
+		ArrayList<Integer> historicalPositions = new ArrayList<Integer>();
+		return historicalPositions;
+	}
+
+}
--- android-client/src/nl/catchvifyoucan/MainActivity.java
+++ android-client/src/nl/catchvifyoucan/MainActivity.java
@@ -2,7 +2,10 @@ package nl.catchvifyoucan;
 
 import java.util.List;
 
+import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import com.google.android.maps.GeoPoint;
@@ -12,31 +15,60 @@ import com.google.android.maps.OverlayItem;
 
 public class MainActivity extends MapActivity {
 
+	LocationManager locationManager;
+	private int longitude = 0;
+	private int latitude = 0;
+	
+	private static final double LOCATIONMULTIPLIER = 1000000.0;
+	
+	
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.main);
 		com.google.android.maps.MapView mapView = (com.google.android.maps.MapView) findViewById(R.id.mainMV);
 		mapView.setBuiltInZoomControls(true);
-
+		
+		/**Here we get the overlay from the map on the view*/
 		List<Overlay> mapOverlays = mapView.getOverlays();
 		Drawable drawable = this.getResources().getDrawable(
 				R.drawable.androidmarker);
-		MyItemizedOverlay itemizedoverlay = new MyItemizedOverlay(
-				drawable, this);
+		MyItemizedOverlay itemizedoverlay = new MyItemizedOverlay(drawable,
+				this);
+
+		setActualPosition();
 		
-		GeoPoint point = new GeoPoint(41889882,12479267);
+		/**Show the item on the current position*/
+		GeoPoint currentPoint = new GeoPoint(latitude, longitude);
+
+		GeoPoint point = new GeoPoint(41889882, 12479267);
 		OverlayItem overlayitem = new OverlayItem(point, "Hey", "I'm in Rome!");
-		GeoPoint point2 = new GeoPoint(52374004,4890359);
-		OverlayItem overlayitem2 = new OverlayItem(point2, "Hallo", "I'm in Amsterdam!");
-		
+		OverlayItem overlayitem2 = new OverlayItem(currentPoint, "Hallo",
+				"I'm in Amsterdam!");
+
 		itemizedoverlay.addOverlay(overlayitem);
 		itemizedoverlay.addOverlay(overlayitem2);
 		mapOverlays.add(itemizedoverlay);
 		
-		
+
 	}
+	
+	/**Instance of the locationmanager to get the lad and lon*/
+	private void setActualPosition(){
+		
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		Location location = locationManager
+				.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		
+		/**Google maps GeoPoint coordinates are specified in microdegrees (degrees * 1e6)*/
+		latitude = (int) (location.getLatitude() * LOCATIONMULTIPLIER);
+		longitude = (int) (location.getLongitude() * LOCATIONMULTIPLIER);
+	}
+
 
 	@Override
 	protected boolean isRouteDisplayed() {
