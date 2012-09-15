da92b436cfee5b1f2c0d945b9102ce647f4a98c1
Lorenzo Marchiori <kioli85@gmail.com>
2012-09-15 13:52:32 +0200

    user insert details

---
+++ android-client/src/nl/catchvifyoucan/UserChoice.java
@@ -0,0 +1,13 @@
+package nl.catchvifyoucan;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UserChoice extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.user_choice);
+	}
+}
