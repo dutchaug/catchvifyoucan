94a1f213e23af06d05eb7149a8c849932ba2ec3d
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-09-15 16:55:18 +0200

    GameController specified
    The GameController responds could respond to the /game and /game/ GET requests.

--- grails-server/grails-app/controllers/catchvifyoucan/grails/server/GameController.groovy
+++ grails-server/grails-app/controllers/catchvifyoucan/grails/server/GameController.groovy
@@ -6,4 +6,17 @@ class GameController {
     def index = {
         redirect(action: "list")
     }
+
+    def allGames = {
+        def games = Game.findAll()
+
+        return [ games: games ]
+    }
+
+    def game = {
+        def game = Game.findById(params.id)
+
+        return [ players: game.players]
+    }
+
 }
---
+++ grails-server/test/unit/catchvifyoucan/grails/server/GameControllerTests.groovy
@@ -0,0 +1,40 @@
+package catchvifyoucan.grails.server
+
+import grails.test.*
+
+class GameControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+        mockDomain(Player)
+
+        mockDomain(Game, [new Game(), new Game()])
+    }
+
+    void testAllGame() {
+        def model = this.controller.allGames()
+
+        assertEquals 2, model["games"].size
+        assertEquals 1, model["games"][0].id
+    }
+
+    void testPlayersInGame() {
+        addPlayersTo(1, [new Player(communicationId: "0"), new Player(communicationId: "1")])
+
+        this.controller.params.id = 1
+        def model = this.controller.game()
+
+        assertEquals 2, model["players"].size()
+    }
+
+    private void addPlayersTo(gameId, players) {
+        def game = Game.findById(gameId)
+        players.each { player ->
+            game.addToPlayers(player)
+        }
+        game.save()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+}
