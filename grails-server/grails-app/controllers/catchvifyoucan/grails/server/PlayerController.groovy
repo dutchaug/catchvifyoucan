f70d2c46f647e5488b4dae5e11a151715922aeb4
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-09-15 18:40:17 +0200

    Controller test are started
    the controller for the game and the player are specified in test.

--- grails-server/grails-app/controllers/catchvifyoucan/grails/server/PlayerController.groovy
+++ grails-server/grails-app/controllers/catchvifyoucan/grails/server/PlayerController.groovy
@@ -6,4 +6,21 @@ class PlayerController {
     def index = {
         redirect(action: "list")
     }
+
+    def trail = {
+        Game game = Game.findById(params.gameId)
+        Player player = Player.findById(params.communicationId)
+
+        return [ trail :  player?.trail.locations.collect {[ latitude : it.latitude, longitude : it.longitude]} ]
+    }
+
+    def location = {
+        Game game = Game.findById(params.gameId)
+        Player player = Player.findById(params.communicationId)
+
+        Location location = player.trail.locations[params.locationIndex]
+
+        return [ longitude: location.longitude, latitude: location.latitude]
+
+    }
 }
---
+++ grails-server/test/unit/catchvifyoucan/grails/server/PlayerControllerTests.groovy
@@ -0,0 +1,50 @@
+package catchvifyoucan.grails.server
+
+import grails.test.*
+
+class PlayerControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+        mockDomain(Game, [new Game()])
+
+        mockDomain(Player, [new Player(communicationId: "0")])
+        Game game = Game.findById(1)
+        game.addToPlayers(Player.findById(1))
+        game.save()
+
+        mockDomain(Trail, [new Trail()])
+        Player player = Player.findById(1)
+        player.trail = Trail.findById(1)
+        player.save()
+
+        mockDomain(Location, [new Location(longitude: 0, latitude: 0), new Location(longitude: 0, latitude: 1), new Location(longitude: 1, latitude: 1)])
+        Location.findAll().each { location ->
+            player.trail.addToLocations(location)
+        }
+        player.save()
+    }
+
+    void testAllLocations() {
+        this.controller.params.gameId = 1
+        this.controller.params.communicationId = 1
+
+        def model = this.controller.trail()
+
+        assertEquals 3, model["trail"].size
+    }
+
+    void testSingleLocations() {
+        this.controller.params.gameId = 1
+        this.controller.params.communicationId = 1
+        this.controller.params.locationIndex = 0
+
+        def model = this.controller.location()
+
+        assertEquals Long.valueOf(0), model["longitude"]
+        assertEquals Long.valueOf(0), model["latitude"]
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+}
