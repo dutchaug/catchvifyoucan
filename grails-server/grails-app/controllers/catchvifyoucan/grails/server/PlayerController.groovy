8d775669c5e70eafd4bbb0b142e9d8b57e9d73c1
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-09-16 10:54:38 +0200

    [REFACTOR] addLocation moved
    In order to support a uniform URL scheme the addLocatoin method is moved from
    TrailsController to the PlayerController.

--- grails-server/grails-app/controllers/catchvifyoucan/grails/server/PlayerController.groovy
+++ grails-server/grails-app/controllers/catchvifyoucan/grails/server/PlayerController.groovy
@@ -33,4 +33,14 @@ class PlayerController {
             game.save()
         //}
     }
+
+    def addLocation = {
+        Game game = Game.findById(params.gameId)
+        Player player = Player.findByPlayerId(params.playerId)
+
+        Location location = new Location(longitude: Long.valueOf(params.longitude), latitude: Long.valueOf(params.latitude))
+
+        player.trail.addToLocations(location)
+        player.save()
+    }
 }
